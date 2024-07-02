0d1bb0bcf11de428db5b2b5c6512f9ff4a069ce1
p4 ★ <144555683+pr4sr@users.noreply.github.com>
2024-07-02 17:08:25 +0300

    Update main.sh

--- libs/main.sh
+++ libs/main.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Define the text with proper spacing
+text=" _ __ ___  __ _ _____   ____ _ _ __ ___ 
+ | '_ ` _ \ / _` |/ __\ \ /\ / / _` | '__/ _ \
+ | | | | | | (_| | (__ \ V V / (_| | | | __/
+ |_| |_| |_|\__,_|\___| \_/\_/ \__,_|_| \___|
+"
+
+# Print the text to the console
+echo "$text"
 
